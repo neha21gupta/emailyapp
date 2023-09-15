f6bb5f33572ea5226b34dac9821836fa26d40481
Nehaa Gupta <nehaagupta126@gmail.com>
2023-09-15 15:02:15 +0530

    Add Initial Files

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    googleClientID: '930293955790-7e01erg34no5g044m7vt8a04ddjptlr3.apps.googleusercontent.com',
+
+    googleClientSecret: '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',
+
+    mongoURI: 'mongodb+srv://neha126:xhKK5ubMeZ8eJ2cV@cluster0.srqtw4q.mongodb.net/emailydev?retryWrites=true&w=majority',
+    cookiekey: 'afghjrtehjdkdjuryrkju'
+    
+};
